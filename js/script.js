6fd93e4b703533967ba498b6ed2a8eb054daccf0
carminenicotera <carmine.91.n@hotmail.it>
2026-02-16 16:19:54 +0100

    selezione elementi dal dom

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//come si fa a generare 5 numeri casuali (magari diversi tra loro) ?
+//come si fa ad attendere 30 secondi (magari nelle prove abbassate questo tempo, così verificate subito se funziona) ?
+//come si fa il confronto tra i numeri generati e i numeri inseriti dall'utente ?
+
+//seleziono gli elementi dal dom
+const countdownEl = document.getElementById('countdown');
+const instructionsEl = document.getElementById('instructions')
+const listEl = document.getElementById('numbers-list');
+const formEl = document.getElementById('answers-form');
+const inputEl = document.querySelectorAll('input')
+const messageEl = document.getElementById('message')
+console.log(inputEl);
